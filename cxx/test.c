6fd4ac76aa38217f0fae27c4dd48c7fa179aacf8
wybosys <wybosys@gmail.com>
2021-04-01 20:23:32 +0800

    cxx

---
+++ cxx/test.c
@@ -0,0 +1,8 @@
+#include "stdio.h"
+
+const char *abc = "abcabcabcabc";
+
+int main()
+{
+    printf("%s", abc);
+}
---
+++ cxx/test.cpp
@@ -0,0 +1,42 @@
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <string>
+
+class A
+{
+public:
+    A(::std::string const &s) : str(s)
+    {
+    }
+    ~A()
+    {
+        ::std::cout << "A Release" << ::std::endl;
+    }
+    ::std::string str;
+};
+
+A *f(const std::vector<A> &v)
+{
+    return (A *)&v.front();
+}
+
+void Test()
+{
+    A *r;
+    {
+        r = f({A("HAHA")});
+    }
+    A &x = *r;
+    ::std::cout << x.str << ::std::endl;
+    x.str = "abc";
+    ::std::cout << x.str << ::std::endl;
+}
+
+::std::string abc = "abcabcabcabc";
+
+int main()
+{
+    ::std::cout << abc << std::endl;
+    return 0;
+}
