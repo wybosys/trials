0335fdd50e3966162ecf4589c4ec879f9207e0ec
wybosys <wybosys@gmail.com>
2021-04-06 15:10:55 +0800

    v2

--- ml/tf-test.py
+++ ml/tf-test.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
-import tensorflow as tf
-# tf.disable_v2_behavior()
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
+
 a = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[2, 3], name='a')
 b = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[3, 2], name='b')
 c = tf.matmul(a, b)
