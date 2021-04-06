5bebc7c76d3b0032047d3022eebd83a721c78929
wybosys <wybosys@gmail.com>
2021-04-06 19:35:34 +0800

    ops

--- ml/tf2-test.py
+++ ml/tf2-test.py
@@ -4,6 +4,7 @@ import tensorflow as tf
 
 a = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[2, 3], name='a')
 b = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[3, 2], name='b')
-c = tf.matmul(a, b)
 
+#c = tf.matmul(a, b)
+c = a @ b
 print(c)
