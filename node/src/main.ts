310a8333e8b82530d4fa7afff87376e82b79c1de
wybosys <wybosys@gmail.com>
2021-09-07 19:28:51 +0800

    增加对promise的保护

--- node/src/main.ts
+++ node/src/main.ts
@@ -1,15 +1,31 @@
-function anno() {
+function anno(err: (e: any) => void = console.error) {
     return (target: any, key: string, desc: PropertyDescriptor) => {
         if (typeof desc.value == 'function') {
             let impl = desc.value
             desc.value = function () {
                 try {
-                    return impl.apply(this, arguments)
+                    let r = impl.apply(this, arguments)
+                    if (r instanceof Promise) {
+                        r.catch(err)
+                        return r
+                    }
+                    return r
                 } catch (e) {
-                    console.error(e)
+                    err(e)
                 }
             }
         }
+    }
+}
+
+class Time {
+
+    static sleep(ms: number): Promise<void> {
+        return new Promise<void>(resolve => {
+            setTimeout(() => {
+                resolve()
+            }, ms)
+        })
     }
 }
 
@@ -21,7 +37,15 @@ class Test {
         throw "A:Exception"
     }
 
+    @anno()
+    async test(msg: string) {
+        console.log(msg)
+        await Time.sleep(1000)
+        throw `T:${msg}`
+    }
+
 }
 
 let t = new Test()
 t.a("hello")
+t.test("async hello")
