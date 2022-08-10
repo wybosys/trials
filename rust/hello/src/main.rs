26cd2279c62187c2af20684cba17568b66b23de5
wybosys <wybosys@gmail.com>
2022-08-10 17:59:03 +0800

    rs

--- rust/hello/src/main.rs
+++ rust/hello/src/main.rs
@@ -1,6 +1,18 @@
 use ferris_says::say;
 use std::io::{stdout, BufWriter};
 
+trait IA {
+    fn a(&self);
+}
+
+struct A {}
+
+impl IA for A {
+    fn a(&self) {
+        println!("A");
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 
@@ -10,4 +22,14 @@ fn main() {
 
     let mut tgt = BufWriter::new(out.lock());
     say(msg.as_bytes(), len, &mut tgt).unwrap();
+
+    let ptr: Option<A>;
+
+    {
+        let a = A {};
+        a.a();
+        ptr = Some(a);
+    }
+
+    ptr.unwrap().a();
 }
