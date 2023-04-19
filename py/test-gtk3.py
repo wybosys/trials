e2f94156026ffb71a011aa3af22cbd7497376eb3
wybosys <wybosys@gmail.com>
2023-04-19 12:15:02 +0800

    tk

--- py/test-gtk3.py
+++ py/test-gtk3.py
@@ -3,3 +3,21 @@
 import gi
 
 gi.require_version("Gtk", "3.0")
+
+from gi.repository import GLib, Gtk
+import sys
+
+
+class MyApplication(Gtk.Application):
+    def __init__(self):
+        super().__init__(application_id="com.example.MyGtkApplication")
+        GLib.set_application_name("My Gtk Application")
+
+    def do_activate(self):
+        window = Gtk.ApplicationWindow(application=self, title="Hello World")
+        window.present()
+
+
+app = MyApplication()
+exit_status = app.run(sys.argv)
+sys.exit(exit_status)
