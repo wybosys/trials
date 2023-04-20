a9c6adfc9acafc64de6d6dda98bafae773579ef2
wybosys <wybosys@gmail.com>
2023-04-20 16:16:28 +0800

    cuda

--- py/dl_gpu_check.py
+++ py/dl_gpu_check.py
@@ -1,24 +1,32 @@
-import tensorflow as tf
-import torch
 import pkg_resources
 import platform
 
-if torch.cuda.is_available():
-    print(torch.cuda.get_device_name())
-    print("\n\ntorch: True")
-else:
-    print("\n\ntorch: False")
+try:
+    import torch
+
+    if torch.cuda.is_available():
+        print(torch.cuda.get_device_name())
+        print("\n\ntorch: True")
+    else:
+        print("\n\ntorch: False")
+except:
+    print("检查torch的安装情况")
 
 print("\n\n")
-if tf.test.is_gpu_available():
-    print(tf.config.list_physical_devices("GPU"))
-    print("\n\ntensorflow: True")
-else:
-    print("\n\ntensorflow: False")
-    if platform.system() == "Windows":
-        ver = pkg_resources.parse_version(tf.version.VERSION)
-        if ver.minor > 10:
-            print("windows上的tensorflow如果需要gpu支持必须安装<=2.10.0")
+try:
+    import tensorflow as tf
 
-if not tf.test.is_built_with_cuda():
-    print("tensorflow-gpu not install")
+    if tf.test.is_gpu_available():
+        print(tf.config.list_physical_devices("GPU"))
+        print("\n\ntensorflow: True")
+    else:
+        print("\n\ntensorflow: False")
+        if platform.system() == "Windows":
+            ver = pkg_resources.parse_version(tf.version.VERSION)
+            if ver.minor > 10:
+                print("windows上的tensorflow如果需要gpu支持必须安装<=2.10.0")
+
+    if not tf.test.is_built_with_cuda():
+        print("tensorflow-gpu not install")
+except:
+    print("检查tensorflow的安装情况")
